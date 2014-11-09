23ffe168c380f1329dc43166d3b1c9835316d4c9
Jake Grimes <hellokitty09041991@hotmail.com>
2014-11-08 23:43:46 -0600

    Changed with: 187.py

--- submit.sh
+++ submit.sh
@@ -1,4 +1,9 @@
-#This script formats the code for submission.
+#This script submits the code to daily programmer 
+#and adds four line spaces for submission to Reddit.
 
-sed 's/^/    /' "$1.py" > temp
+git add $1
+git add submit.sh
+git commit -m "Changed with: $1"
+git push
+sed 's/^/    /' "$1" > temp
 subl temp
